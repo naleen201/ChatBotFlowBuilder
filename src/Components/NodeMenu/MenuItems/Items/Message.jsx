0bdffa60024c8508a696a7207a4963b752df74f6
Naleen <57667084+naleen201@users.noreply.github.com>
2024-05-20 14:15:37 +0530

    Changed default message for a new Message Node
    Changed default message for a new Message Node

--- src/Components/NodeMenu/MenuItems/Items/Message.jsx
+++ src/Components/NodeMenu/MenuItems/Items/Message.jsx
@@ -3,7 +3,7 @@ import MenuItem from '../MenuItem'
 
 function Message() {
   return (
-    <MenuItem nodeType={'messageNode'} nodeData={{nodeName: 'Message', message : 'TestMessage'}}><div>Message</div></MenuItem>
+    <MenuItem nodeType={'messageNode'} nodeData={{nodeName: 'Message', message : 'Click to edit this message!'}}><div>Message</div></MenuItem>
   )
 }
 
